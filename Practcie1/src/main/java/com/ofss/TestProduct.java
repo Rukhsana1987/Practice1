e6d23266ea239a2b4f326bc7b6a4d9c32404e1e3
Rukhsana <rukhsanaibn@gmail.com>
2021-02-22 12:12:52 +0530

    Adding the modified Java file

--- Practcie1/src/main/java/com/ofss/TestProduct.java
+++ Practcie1/src/main/java/com/ofss/TestProduct.java
@@ -1,54 +1,14 @@
 package com.ofss;
 
-public class Stock {
+public class TestProduct {
 
-	int stockId;
-	String stockName;
-	double stockPrice;
-	
-	public Stock() {
-		super();
-		// TODO Auto-generated constructor stub
+	public static void main(String[] args) {
+		System.out.println("Starting the application");
+		Stock st1=new Stock(100, "ORCL", 123.4);
+		System.out.println(st1);
+		System.out.println("=========================");
+				
+
 	}
 
-	public Stock(int stockId, String stockName, double stockPrice) {
-		super();
-		this.stockId = stockId;
-		this.stockName = stockName;
-		this.stockPrice = stockPrice;
-	}
-
-	public int getStockId() {
-		return stockId;
-	}
-
-	public void setStockId(int stockId) {
-		this.stockId = stockId;
-	}
-
-	public String getStockName() {
-		return stockName;
-	}
-
-	public void setStockName(String stockName) {
-		this.stockName = stockName;
-	}
-
-	public double getStockPrice() {
-		return stockPrice;
-	}
-
-	public void setStockPrice(double stockPrice) {
-		this.stockPrice = stockPrice;
-	}
-
-	@Override
-	public String toString() {
-		return "Stock [stockId=" + stockId + ", stockName=" + stockName + ", stockPrice=" + stockPrice + "]";
-	}
-	
-	
-	
-	
-	
 }
